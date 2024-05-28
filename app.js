e52d728f7b017c992e2f4723fb5d66f655cbb31f
ALIZAIQBAL <alizaiqbal322@gmail.com>
2024-05-28 17:52:46 +0500

    Message

---
+++ app.js
@@ -0,0 +1,14 @@
+var things=["dress","shoes","jwellery","cosmetics","purse"]
+// things.pop()
+// things.push("car")
+// things.shift()
+// things.unshift("qawali")
+things.splice(1,2,"rose")
+document.write(things)
+var manufacture=["apple","samsung","ipad","motrolla"];
+document.write("<select>")
+document.write('<option value="'+manufacture[0]+'">'+ manufacture[0] + '</option>')
+document.write('<option value="'+manufacture[1]+'">'+ manufacture[1] + '</option>')
+document.write('<option value="'+manufacture[2]+'">'+ manufacture[2] + '</option>')
+document.write('<option value="'+manufacture[3]+'">'+ manufacture[3] + '</option>')
+document.write("</select>")
